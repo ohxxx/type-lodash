168b6f5fd9ad5fc6da0768d63e1b49039c65c3cc
xxxDeveloper <humengmingxx@gmail.com>
2022-03-03 10:23:26 +0800

    feat(string)：add split implementation steps

--- lib/string/split.d.ts
+++ lib/string/split.d.ts
@@ -1,15 +1,44 @@
 /**
- * Build string array
+ * [Step 1]: Build string array
  * 
  * @example
  * BuildStringArray<[], 'xxx'>
  * >>> ['xxx']
  */
 type BuildStringArray<Arr extends string[], Item extends string> = [...Arr, Item]
+// type Test = BuildStringArray<[], '1' | '2'>
+// >>> ['1', '2']
 
 
 /**
- * Splits string by separator
+ * [Step 2]: Match Delimiter
+ * 
+ * @example
+ * MatchDelimiter<'0-0', '-'>
+ * >>> true
+ */
+// type MatchDelimiter<
+//   Str extends string,
+//   Delimiter extends string = ''
+// > =
+//   Str extends `${infer First}${Delimiter}${infer Rest}`
+//     ? true
+//     : false
+// type Test = MatchDelimiter<'xxx-xxx', '-'>
+// >>> true
+
+
+
+/**
+ * [Ultimate]: Splits string by separator
+ * 
+ * Implementation steps:
+ *    1、Determine if assignable - mixed delimiter strings
+ *        a、Yes - recurse string, continue matching
+ *        b、No - continue to step 2
+ *    2、Determine whether to assign - separate delimiter
+ *        a、Yes - returns the current result set
+ *        b、No - put the last string into the result set
  * 
  * @example
  * Split<'xxx_xxx', '_'>
