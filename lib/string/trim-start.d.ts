9e0bd7c13b5c5c65cc0dc126abfe15feba7d44d7
xxxDeveloper <humengmingxx@gmail.com>
2022-03-04 15:03:16 +0800

    feat(trim-start)：done

---
+++ lib/string/trim-start.d.ts
@@ -0,0 +1,23 @@
+/**
+ * 移除前面的空格或者指定字符
+ * 
+ * 注：这里处理方式和 lodash 中的处理方式不太一样
+ * lodash 中 TrimStart 是按照单独字符类型 char 删除，而不是 字符串类型 string 模式匹配删除的
+ * (具体详情：https://github.com/lodash/lodash/issues/2904) 
+ * 而个人感觉按照字符串模式匹配删除才是我想要的，所以将字符串模式匹配删除完成以下功能(TrimEnd同)
+ * 
+ * 实现思路：直接使用类型推断进行类型分配判断
+ * 
+ * @example
+ * type Test = TrimStart<'  abc '>
+ * >>> "abc "
+ */
+export type TrimStart<
+  Str extends string,
+  Chars extends string = ' '
+> = 
+  Str extends `${Chars}${infer Rest}`
+    ? TrimStart<Rest, Chars>
+    : Str
+
+type Test = TrimStart<'  abc '>
---
+++ test-d/string/trim-start.ts
@@ -0,0 +1,15 @@
+import { expectType } from 'tsd'
+import type { TrimStart } from '../../lib/string/trim-start'
+
+declare function trimStart<
+  Str extends string,
+  Chars extends string = ' '
+>(str: Str, chars?: Chars): TrimStart<Str, Chars>
+
+expectType<'abc'>(trimStart('abc'))
+expectType<'abc  '>(trimStart('  abc  '))
+expectType<'_abc_ '>(trimStart(' _abc_ '))
+expectType<'babc'>(trimStart('aababc', 'a'))
+expectType<'abc'>(trimStart('abc', 'c'))
+expectType<''>(trimStart('-_-abc-_-', '-_-abc-_-'))
+expectType<'-_-abc-_-'>(trimStart('-_-abc-_-', '_-'))
