1126217d623a4557dc1d51fb71b206d8f59aa0e3
xxxDeveloper <humengmingxx@gmail.com>
2022-03-12 23:44:58 +0800

    feat(constant)：up english chars

--- lib/helpers/constant.d.ts
+++ lib/helpers/constant.d.ts
@@ -3,7 +3,18 @@
  * 
  * 可以用来进行英文字符的模式匹配
  */
-export type EnglishChars = 'a' | 'b' | 'c' | 'd' | 'e' | 'f' | 'g' | 'h' | 'i' | 'j' | 'k' | 'l' | 'm' | 'n' | 'o' | 'p' | 'q' | 'r' | 's' | 't' | 'u' | 'v' | 'w' | 'x' | 'y' | 'z' | 'A' | 'B' | 'C' | 'D' | 'E' | 'F' | 'G' | 'H' | 'I' | 'J' | 'K' | 'L' | 'M' | 'N' | 'O' | 'P' | 'Q' | 'R' | 'S' | 'T' | 'U' | 'V' | 'W' | 'X' | 'Y' | 'Z'
+export type EnglishChars = LowercaseChars | UppercaseChars
+
+/**
+ * 小写英文字符
+ */
+export type LowercaseChars = 'a' | 'b' | 'c' | 'd' | 'e' | 'f' | 'g' | 'h' | 'i' | 'j' | 'k' | 'l' | 'm' | 'n' | 'o' | 'p' | 'q' | 'r' | 's' | 't' | 'u' | 'v' | 'w' | 'x' | 'y' | 'z' 
+
+
+/**
+ * 大写英文字符
+ */
+export type UppercaseChars = 'A' | 'B' | 'C' | 'D' | 'E' | 'F' | 'G' | 'H' | 'I' | 'J' | 'K' | 'L' | 'M' | 'N' | 'O' | 'P' | 'Q' | 'R' | 'S' | 'T' | 'U' | 'V' | 'W' | 'X' | 'Y' | 'Z'
 
 /**
  * HTML实体字符
