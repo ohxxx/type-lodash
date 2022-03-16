f2e7e276aeb52193f4485e24b1ccf252abbe7e62
xxxDeveloper <humengmingxx@gmail.com>
2022-03-16 22:48:59 +0800

    feat(camel-case)：done

--- lib/string/camel-case.ts
+++ lib/string/camel-case.ts
@@ -1,7 +1,140 @@
-import type { ASCIISymbol } from "../helpers/constant";
+import type { ASCIISymbol, ArabicFigures, EnglishChars, UppercaseChars, LowercaseChars } from "../helpers/constant";
 import type { LowerFirst } from "./lower-first";
 import type { ToLower } from "./to-lower";
 import type { ToUpper } from "./to-upper";
+/**
+ * 符号 + 符号
+ */
+type MatchAllASCIISymbol<
+  LeftStr extends string,
+  RightStr extends string
+> = `${LeftStr}${RightStr}` extends `${ASCIISymbol}${ASCIISymbol}`
+  ? true
+  : false
+
+/**
+ * 符号 + 英文
+ */
+type MatchSymbolEnglish<
+  LeftStr extends string,
+  RightStr extends string
+> = `${LeftStr}${RightStr}` extends `${ASCIISymbol}${EnglishChars}`
+  ? true
+  : false
+
+/**
+ * 符号 + 数字
+ */
+type MatchSymbolNumber<
+  LeftStr extends string,
+  RightStr extends string
+> = `${LeftStr}${RightStr}` extends `${ASCIISymbol}${ArabicFigures}`
+  ? true
+  : false
+
+/**
+ * 英文 + 符号
+ */
+type MatchEnglishSymbol<
+  LeftStr extends string,
+  RightStr extends string
+> = `${LeftStr}${RightStr}` extends `${EnglishChars}${ASCIISymbol}`
+  ? true
+  : false
+
+/**
+ * 数字 + 符号
+ */
+type MatchNumberSymbol<
+  LeftStr extends string,
+  RightStr extends string
+> = `${LeftStr}${RightStr}` extends `${ArabicFigures}${ASCIISymbol}`
+  ? true
+  : false
+
+/**
+ * 数字 + 英文
+ */
+type MatchNumberEnglish<
+  LeftStr extends string,
+  RightStr extends string
+> = `${LeftStr}${RightStr}` extends `${ArabicFigures}${EnglishChars}` 
+  ? true
+  : false
+
+/**
+ * 英文 + 数字
+ */
+type MatchEnglishNumber<
+  LeftStr extends string,
+  RightStr extends string
+> = `${LeftStr}${RightStr}` extends `${EnglishChars}${ArabicFigures}` 
+  ? true
+  : false
+
+/**
+ * 数字 + 数字
+ */
+type MatchAllNumber<
+  LeftStr extends string,
+  RightStr extends string
+> = `${LeftStr}${RightStr}` extends `${ArabicFigures}${ArabicFigures}` 
+  ? true
+  : false
+
+
+/**
+ * 大写 + 大写
+ */
+type MatchAllUpperCase<
+  LeftStr extends string,
+  RightStr extends string
+> = `${LeftStr}${RightStr}` extends `${UppercaseChars}${UppercaseChars}` 
+  ? true
+  : false
+
+/**
+ * 小写 + 小写
+ */
+type MatchAllLowercase<
+  LeftStr extends string,
+  RightStr extends string
+> = `${LeftStr}${RightStr}` extends `${LowercaseChars}${LowercaseChars}` 
+  ? true
+  : false
+
+/**
+ * 小写 + 大写
+ */
+type MatchLowerUpper<
+  LeftStr extends string,
+  RightStr extends string
+> = `${LeftStr}${RightStr}` extends `${LowercaseChars}${UppercaseChars}` 
+  ? true
+  : false
+
+/**
+ * 其他字符
+ */
+type OtherSymbol<Str extends string> =
+  Str extends EnglishChars
+  ? false
+  : Str extends ASCIISymbol
+    ? false
+    : Str extends ArabicFigures
+      ? false
+      : true
+
+/**
+ * 大写 + 其他（排除数字、英文、已知符号）
+ */
+type MatchUppercaseOther<
+  LeftStr extends string,
+  RightStr extends string
+> = `${LeftStr}${RightStr}` extends `${UppercaseChars}${infer Rest}` 
+  ? OtherSymbol<Rest>
+  : false
+
 
 /**
  * 将字符串转换为驼峰式大小写
@@ -12,7 +145,18 @@ import type { ToUpper } from "./to-upper";
  * 所以这里我准备将所有出现的规则列举出，并进行模式匹配处理
  * 
  * 出现场景：
- *    1、
+ *    1、符号 + 符号
+ *    2、符号 + 英文
+ *    3、符号 + 数字
+ *    4、英文 + 符号
+ *    5、数字 + 符号
+ *    6、数字 + 英文
+ *    7、英文 + 数字
+ *    8、数字 + 数字
+ *    9、大写英文 + 大写英文
+ *    10、小写英文 + 小写英文
+ *    11、小写英文 + 大写英文
+ *    12、大写英文 + 其他
  * 
  * 
  * 实现思路：（整体思路如上述）
@@ -21,18 +165,35 @@ import type { ToUpper } from "./to-upper";
  */
 type _CamelCase<Str extends string> =
   Str extends `${infer First}${infer Second}${infer Rest}`
-    ? First extends ASCIISymbol
-      ? Second extends ASCIISymbol
-        ? _CamelCase<`${Second}${Rest}`>
-        : _CamelCase<`${ToUpper<Second>}${Rest}`>
-      : `${First}${_CamelCase<`${ToLower<Second>}${Rest}`>}`
+    ? MatchAllASCIISymbol<First, Second> extends true
+      ? _CamelCase<Rest>
+      : MatchAllNumber<First, Second> extends true
+        ? `${First}${_CamelCase<`${Second}${Rest}`>}`
+        : MatchAllUpperCase<First, Second> extends true
+          ? `${ToLower<First>}${_CamelCase<`${Second}${Rest}`>}`
+          : MatchAllLowercase<First, Second> extends true
+            ? `${First}${_CamelCase<`${Second}${Rest}`>}`
+            : MatchSymbolEnglish<First, Second> extends true
+              ? `${ToUpper<Second>}${_CamelCase<Rest>}`
+              : MatchSymbolNumber<First, Second> extends true
+                ? `${_CamelCase<`${Second}${Rest}`>}`
+                : MatchEnglishSymbol<First, Second> extends true
+                  ? `${ToLower<First>}${_CamelCase<`${Second}${Rest}`>}`
+                  : MatchNumberSymbol<First, Second> extends true
+                    ? `${First}${_CamelCase<`${Second}${Rest}`>}`
+                    : MatchNumberEnglish<First, Second> extends true
+                      ? `${First}${ToUpper<Second>}${_CamelCase<`${Rest}`>}`
+                      : MatchEnglishNumber<First, Second> extends true
+                        ? `${ToLower<First>}${_CamelCase<`${Second}${Rest}`>}`
+                        : MatchLowerUpper<First, Second> extends true
+                          ? `${First}${Second}${_CamelCase<`${Rest}`>}`
+                          : MatchUppercaseOther<First, Second> extends true
+                            ? `${ToLower<First>}${_CamelCase<`${Second}${Rest}`>}`
+                            : `${First}${Second}${_CamelCase<Rest>}`
     : Str extends ASCIISymbol
       ? ''
-      : Str
+      : Str extends UppercaseChars
+        ? ToLower<Str>
+        : Str
 
-export type CamelCase<Str extends string> =
-  LowerFirst<_CamelCase<Str>>
-
-// TODO：大小写场景问题
-type Test = CamelCase<'AaBbCc'>
-// aaBbCc
+export type CamelCase<Str extends string> = LowerFirst<_CamelCase<Str>>
