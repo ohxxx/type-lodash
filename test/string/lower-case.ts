6f6627551134b84df94b3e0167df0ae77c88b704
xxxDeveloper <humengmingxx@gmail.com>
2022-03-18 23:28:40 +0800

    test(lower-case)：init

---
+++ test/string/lower-case.ts
@@ -0,0 +1,27 @@
+import { expectType, expectError } from 'tsd';
+import type { LowerCase } from '../../lib/string/lower-case'
+
+declare function lowerCase<Str extends string>(str: Str): LowerCase<Str>
+
+expectType<''>(lowerCase(''))
+expectType<'foo bar'>(lowerCase('    FOO    BAR    '))
+expectType<'foo bar'>(lowerCase('--Foo-Bar--'))
+expectType<'foo bar'>(lowerCase('fooBar'))
+expectType<'foo bar'>(lowerCase('__Foo_Bar__'))
+expectType<'（ foo bar ）'>(lowerCase('--（(FOO-Bar)）--'))
+expectType<'（ foo 6 a aaa 6 aa 6 bar ）'>(lowerCase('--（(Foo6aAaa6AA6Bar)）--'))
+
+expectType<'aaaaaa'>(lowerCase('AAAAAA'))
+expectType<'aaaaaa'>(lowerCase('aaaaaa'))
+expectType<'a aaaaa'>(lowerCase('AAaaaa'))
+expectType<'aa aaaa'>(lowerCase('AAAaaa'))
+expectType<'aaa aaa'>(lowerCase('AAAAAa'))
+expectType<'aaaa aa'>(lowerCase('AaaaAA'))
+expectType<'aaa aaa'>(lowerCase('AaaAAA'))
+expectType<'aa aaaa'>(lowerCase('AaAAAA'))
+expectType<'aaaabccc bbbbccd dd dd dcc dc ddddcc cc c'>(lowerCase('AaaabcccBBBBCCDDdDDDccDcDDDDCCCcC'))
+
+
+expectError(lowerCase())
+expectError(lowerCase([]))
+expectError(lowerCase(123))
