d1d5b4523029c51d72bf5f462242e074a237e3dc
xxxDeveloper <humengmingxx@gmail.com>
2022-03-30 22:47:51 +0800

    test(compact)：up

--- test/array/compact.ts
+++ test/array/compact.ts
@@ -1,11 +1,11 @@
 import { expectError, expectType } from 'tsd';
 import type { Compact } from '../../lib/array/compact'
 
-declare function compact<A extends unknown[], R extends unknown[]>(arr: A) : Compact<A, R>
+declare function compact<A extends unknown[]>(arr: A) : Compact<A>
 
 expectType<[]>(compact([]))
 expectType<[]>(compact([0, false, null, undefined, ""]))
-expectType<[1, 2, true, "3"]>(compact([1, 2, 0, false, true, null, undefined, "", "3"]))
+expectType<Compact<[1, 2, 0, false, true, null, undefined, "", "3"]>>([1, 2, true, "3"])
 
 expectError(compact(''))
 expectError(compact(false))
