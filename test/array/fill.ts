47748036a500a90ad83815928f9390905fb85e41
xxxDeveloper <humengmingxx@gmail.com>
2022-04-18 23:39:12 +0800

    test(fill)：up cases

--- test/array/fill.ts
+++ test/array/fill.ts
@@ -1,37 +1,30 @@
 import { expectType } from 'tsd';
 import type { Fill } from '../../lib/array/fill'
-
-declare function fill<
-  Arr extends unknown[],
-  Value extends unknown,
-  Start extends number,
-  End extends number
->(arr: Arr, value: Value, start: Start, end: End): Fill<Arr, Value, Start, End>
     
-expectType<[]>(fill([], '', 0, 0))
+expectType<Fill<[], '', 0, 0>>([])
 
-expectType<[]>(fill([], [1]))
-expectType<[[1], [1], [1]]>(fill([1, 2, 3], [1]))
-expectType<[2, 2, 2]>(fill([1, 2, 3], 2))
-expectType<[true, true, true]>(fill([1, 2, 3], true))
+expectType<Fill<[], [1]>>([])
+expectType<Fill<[1, 2, 3], [1]>>([[1], [1], [1]])
+expectType<Fill<[1, 2, 3], 2>>([2, 2, 2])
+expectType<Fill<[1, 2, 3], true>>([true, true, true])
 
-expectType<['x', 'x', 'x']>(fill([1, 2, 3], 'x'))
-expectType<['x', 'x', 'x']>(fill([1, 2, 3], 'x', 0))
-expectType<[1, 'x', 'x']>(fill([1, 2, 3], 'x', 1))
-expectType<[1, 2, 'x']>(fill([1, 2, 3], 'x', 2))
-expectType<[1, 2, 3]>(fill([1, 2, 3], 'x', 3))
+expectType<Fill<[1, 2, 3], 'x'>>(['x', 'x', 'x'])
+expectType<Fill<[1, 2, 3], 'x', 0>>(['x', 'x', 'x'])
+expectType<Fill<[1, 2, 3], 'x', 1>>([1, 'x', 'x'])
+expectType<Fill<[1, 2, 3], 'x', 2>>([1, 2, 'x'])
+expectType<Fill<[1, 2, 3], 'x', 3>>([1, 2, 3])
 
-expectType<['x', 'x', 'x']>(fill([1, 2, 3], 'x', 0, 3))
-expectType<[1, 'x', 'x']>(fill([1, 2, 3], 'x', 1, 3))
-expectType<[1, 2, 'x']>(fill([1, 2, 3], 'x', 2, 3))
-expectType<[1, 2, 3]>(fill([1, 2, 3], 'x', 3, 3))
-expectType<[1, 2, 3]>(fill([1, 2, 3], 'x', 4, 3))
+expectType<Fill<[1, 2, 3], 'x', 0, 3>>(['x', 'x', 'x'])
+expectType<Fill<[1, 2, 3], 'x', 1, 3>>([1, 'x', 'x'])
+expectType<Fill<[1, 2, 3], 'x', 2, 3>>([1, 2, 'x'])
+expectType<Fill<[1, 2, 3], 'x', 3, 3>>([1, 2, 3])
+expectType<Fill<[1, 2, 3], 'x', 4, 3>>([1, 2, 3])
 
-expectType<[1, 2, 'x']>(fill([1, 2, 3], 'x', -1))
-expectType<[1, 'x', 'x']>(fill([1, 2, 3], 'x', -2))
-expectType<['x', 'x', 'x']>(fill([1, 2, 3], 'x', -3))
+expectType<Fill<[1, 2, 3], 'x', -1>>([1, 2, 'x'])
+expectType<Fill<[1, 2, 3], 'x', -2>>([1, 'x', 'x'])
+expectType<Fill<[1, 2, 3], 'x', -3>>(['x', 'x', 'x'])
 
-expectType<[1, 2, 3]>(fill([1, 2, 3], 'x', -1, 2))
-expectType<[1, 2, 'x']>(fill([1, 2, 3], 'x', -1, 5))
-expectType<[1, 'x', 3]>(fill([1, 2, 3], 'x', -2, 2))
-expectType<[1, 'x', 'x']>(fill([1, 2, 3], 'x', -2, 3))
+expectType<Fill<[1, 2, 3], 'x', -1, 2>>([1, 2, 3])
+expectType<Fill<[1, 2, 3], 'x', -1, 5>>([1, 2, 'x'])
+expectType<Fill<[1, 2, 3], 'x', -2, 2>>([1, 'x', 3])
+expectType<Fill<[1, 2, 3], 'x', -2, 3>>([1, 'x', 'x'])
