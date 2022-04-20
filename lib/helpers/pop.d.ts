371efc1ccb1c4266c51b79bdd866702e8ca17eb4
xxxDeveloper <humengmingxx@gmail.com>
2022-04-20 23:40:47 +0800

    fix(pop)：repair function

--- lib/helpers/pop.d.ts
+++ lib/helpers/pop.d.ts
@@ -1,15 +1,15 @@
 /**
- * 删除元组的最后一个元素，并返回该元素
+ * 删除元组的最后一个元素，并返回新元组
  * 
  * @param { unknown[] } Arr - 需要操作的元组
  * @return { unknown[] } 返回操作后的元组
  * 
  * @example
  * type Test = Pop<[1, '2', 3]>
- * >>> 3
+ * >>> [1, "2"]
  */
 export type Pop<Arr extends unknown[]> = 
-  Arr extends [...infer _Other, infer Last]
-    ? Last
-    : undefined
-
+  Arr extends [...infer Other, any]
+    ? Other
+    : []
+    
