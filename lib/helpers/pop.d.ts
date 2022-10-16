018237dae789841912495a1080ad52ac23f807ef
xxx <humengmingxx@gmail.com>
2022-10-16 19:49:30 +0800

    fix(pop)：up

--- lib/helpers/pop.d.ts
+++ lib/helpers/pop.d.ts
@@ -1,15 +1,15 @@
 /**
- * 删除元组的最后一个元素，并返回新元组
+ * 删除元组的最后一个元素，并返回改该元素
  * 
  * @param { unknown[] } Arr - 需要操作的元组
- * @return { unknown[] } 返回操作后的元组
+ * @return { unknown } 返回删除的元素
  * 
  * @example
  * type Test = Pop<[1, '2', 3]>
- * >>> [1, "2"]
+ * >>> 3
  */
 export type Pop<Arr extends unknown[]> = 
-  Arr extends [...infer Other, any]
-    ? Other
+  Arr extends [...any, infer Last]
+    ? Last
     : []
     
--- test/helpers/pop.ts
+++ test/helpers/pop.ts
@@ -2,4 +2,4 @@ import { expectType } from 'tsd';
 import type { Pop } from '../../lib/helpers/pop';
 
 expectType<Pop<[]>>([])
-expectType<Pop<[1, '2', 3]>>([1, '2'])
+expectType<Pop<[1, '2', 3]>>(3)
